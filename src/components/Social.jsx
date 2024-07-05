5daa72dc44af3387e3bd6161c6cd2211157e9436
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-07-05 19:29:50 +0530

    container done

---
+++ src/components/Social.jsx
@@ -0,0 +1,10 @@
+
+export default function Social() {
+  return (
+    <div className="h-screen w-screen bg-stone-950 flex justify-center items-center">
+        <div className="h-3/5 w-1/5 bg-neutral-800 rounded-lg">
+
+        </div>
+    </div>
+  )
+}
