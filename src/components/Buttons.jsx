61e7df16b51b17b38dca46e5facd835641031e8a
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-07-06 00:27:46 +0530

    finished

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -5,7 +5,7 @@ export default function Buttons() {
     <>
       {links.map((btn, index) => {
           return (
-              <div key={index} className="bg-Grey w-5/6 h-20 rounded-md flex justify-center items-center text-white font-bold py-1 active:bg-custom-yellow active:text-black cursor-pointer select-none">
+              <div key={index} className="bg-Grey w-5/6 md:h-20 rounded-md flex justify-center items-center text-white font-semibold py-1  hover:bg-custom-yellow hover:text-black cursor-pointer select-none">
                   <button>
                       {btn}
                     </button>
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -2,15 +2,15 @@ import Buttons from "./Buttons";
 export default function Social() {
   return (
     <div className="h-screen w-screen bg-Off-Black flex justify-center items-center text-sm font-inter">
-        <div className="h-3/5 md:w-80 bg-Dark-Grey rounded-lg">
-            <div className="h-1/2 w-full flex flex-col gap-1 items-center pt-10">
+        <div className="h-4/5 w-96 mx-2 md:h-3/5 md:w-80 bg-Dark-Grey rounded-lg select-none">
+            <div className="h-1/2 w-full flex flex-col gap-1 items-center pt-5 md:pt-10">
                 <img 
                 src="./src/assets/image.jpg" 
                 alt="Profile Image" 
-                className="h-24 w-24 rounded-full"/>
+                className="md:h-24 md:w-24 h-16 w-16 rounded-full"/>
                 <h3 className="mt-2 text-xl font-bold text-white">Saptarsi Debnath</h3>
                 <p className="font-bold text-custom-yellow">Kolkata, India</p>
-                <p className="mt-3 text-white font-semibold">&quot;Front-end developer and avid reader.&quot;</p>
+                <p className="mt-3 text-white font-normal">&quot;Front-end developer and avid reader.&quot;</p>
             </div>
             <div className="h-1/2 w-full flex flex-col gap-4 pb-8 items-center justify-between">
                 <Buttons/>
